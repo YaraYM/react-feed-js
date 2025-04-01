72d8f9d675fd93461c643a1f7911804a9f4a404e
Yara Yamanaka Marques <yaraym0408@gmail.com>
2025-04-01 11:44:05 -0300

    feat(sidebar): cria componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post 
-        author="Tony Corleone" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eveniet nemo reprehenderit? Porro quibusdam architecto amet distinctio laborum consectetur quia deleniti iste similique dicta aspernatur neque, voluptatem velit natus reprehenderit." 
-      />
-      <Post
-        author="Ipa Corlone"
-        content="Birimbimbimbimbimbimbim"
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Tony Corleone"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eveniet nemo reprehenderit? Porro quibusdam architecto amet distinctio laborum consectetur quia deleniti iste similique dicta aspernatur neque, voluptatem velit natus reprehenderit."
+          />
+          <Post
+            author="Ipa Corlone"
+            content="Birimbimbimbimbimbimbim"
+          />
+        </main>
+      </div>
     </div>
   )
 }
