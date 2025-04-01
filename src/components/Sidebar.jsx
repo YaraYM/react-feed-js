c304bf2f9fcefc4e50fa5316ea4890d80b8794bd
Yara Yamanaka Marques <yaraym0408@gmail.com>
2025-04-01 12:07:05 -0300

    chore(sidebar: finaliza sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,18 @@ export function Sidebar() {
             />
         
             <div className={styles.profile}>
+                <img 
+                    className={styles.avatar} 
+                    src="https://scontent.fcpq1-1.fna.fbcdn.net/v/t1.6435-1/130551099_104040914916928_7863492300930494640_n.jpg?stp=c0.0.960.960a_dst-jpg_s200x200_tt6&_nc_cat=105&ccb=1-7&_nc_sid=e99d92&_nc_ohc=hOXpMsAtcO4Q7kNvgH8JXHL&_nc_oc=AdlZEp3WhJP-oemDPyvu0XkULgDlOwzmnO8ymf0aqoLVzTp4EiS6Q2uGS0bnm4OI4Wo&_nc_zt=24&_nc_ht=scontent.fcpq1-1.fna&_nc_gid=-aPx90_PAVlI8EvgVFVBMA&oh=00_AYFdMSCah4kWxQ0RnObKZlst32PvY8yOYyUkpdOXFG5jnw&oe=6813655E" 
+                />
+
                 <strong>Tony Corleone</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
