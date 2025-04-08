bed2127b081ce1044fee11f196f9a0218d98574b
Yara Yamanaka Marques <yaraym0408@gmail.com>
2025-04-08 14:15:20 -0300

    feat(comment): cria componente

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/YaraYM.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Yara Yamanaka Marques</strong>
+                            <time title='02 de fevereiro de 2025 às 8h13' dateTime='2025-02-02 11:05:23'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom, Tony! Parabéns!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
