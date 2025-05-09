26ef6a388414b61e4f2350c4097d80ced14310c1
Yara Yamanaka Marques <yaraym0408@gmail.com>
2025-05-09 08:13:16 -0300

    feat(avatar): cria e estiliza componente

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,10 +12,7 @@ export function Sidebar() {
             />
         
             <div className={styles.profile}>
-                <img 
-                    className={styles.avatar} 
-                    src="https://scontent.fcpq1-1.fna.fbcdn.net/v/t1.6435-1/130551099_104040914916928_7863492300930494640_n.jpg?stp=c0.0.960.960a_dst-jpg_s200x200_tt6&_nc_cat=105&ccb=1-7&_nc_sid=e99d92&_nc_ohc=hOXpMsAtcO4Q7kNvgH8JXHL&_nc_oc=AdlZEp3WhJP-oemDPyvu0XkULgDlOwzmnO8ymf0aqoLVzTp4EiS6Q2uGS0bnm4OI4Wo&_nc_zt=24&_nc_ht=scontent.fcpq1-1.fna&_nc_gid=-aPx90_PAVlI8EvgVFVBMA&oh=00_AYFdMSCah4kWxQ0RnObKZlst32PvY8yOYyUkpdOXFG5jnw&oe=6813655E" 
-                />
+                <Avatar src="https://github.com/yaraym.png" />
 
                 <strong>Tony Corleone</strong>
                 <span>Web Developer</span>
